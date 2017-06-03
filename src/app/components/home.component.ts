d5a1ad76907340d25f301b34a4576840df0f3cea
Anna <karlovskaya@gmail.com>
2017-06-03 16:00:45 -0700

    add home page style and images

---
+++ src/app/components/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'home-content',
+  templateUrl: './home.component.html',
+  styleUrls: ['../css/home.css']
+})
+export class HomeComponent { }
