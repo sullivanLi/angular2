7e15b20f99ce689b8fe492ea871fdb3c9a48088f
Sullivan Lee <sullivan1982@gmail.com>
2017-06-23 10:56:53 -0700

    add prototype of product page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,21 @@ import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { TaxonComponent }  from './components/taxon.component';
 import { TaxonProductComponent }  from './components/taxon.product.component';
 import { HeaderComponent }  from './components/header.component';
 import { FooterComponent }  from './components/footer.component';
 import { HomeComponent }  from './components/home.component';
+import { ProductComponent }  from './components/product.component';
 
 const appRoutes: Routes = [
-  { path: '', component: TaxonComponent, pathMatch: 'full' },
-  { path: 'taxons/:id', component: TaxonProductComponent }
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'taxons/:id', component: TaxonProductComponent },
+  { path: 'products/:id', component: ProductComponent }
 ];
 
 @NgModule({
   imports:      [ RouterModule.forRoot(appRoutes), BrowserModule, HttpModule ],
-  declarations: [ AppComponent, TaxonComponent, TaxonProductComponent, HeaderComponent, FooterComponent, HomeComponent ],
+  declarations: [ AppComponent, TaxonProductComponent, HeaderComponent, FooterComponent, HomeComponent, ProductComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
